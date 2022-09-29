b9c41503313796c71330a9d42716bb1089377dd8
MarQuerz <marco.querzola@griffithuni.edu.au>
2022-09-29 12:25:26 +1000

    Started GUI
    Can load the csv file and view data

--- database.py
+++ database.py
@@ -12,18 +12,73 @@
 # print(dataset)
 
 
-import sys
-import os
-import time
-import sqlite3
-import re
-import wx
-import wx.lib.mixins.inspection
-import wx.lib.mixins.listctrl as listmix
+import tkinter as tk
+from tkinter import filedialog, messagebox, ttk
 
-AppTitle = "NSW Traffic Penaly Data"
+import pandas as pd
 
-class MyConnection(object):
-    def __init__(self):
-        self.database_dir = wx.GetApp().GetDatabaseDir()
-        dbFile = os.path.join(self.database_dir, "penalty_data_set_2.csv")
+root = tk.Tk()
+root.geometry("500x500")
+root.pack_propagate(False)
+root.resizable(0, 0)
+
+frame1 = tk.LabelFrame(root, text="CSV Data")
+frame1.place(height=250, width=500)
+
+file_frame = tk.LabelFrame(root, text="Open File")
+file_frame.place(height=100, width=400, rely=0.65, relx=0)
+
+button1 = tk.Button(file_frame, text="Browse A File", command=lambda: file_dialog())
+button1.place(rely=0.65, relx=0.5)
+
+button2 = tk.Button(file_frame, text="Load File", command=lambda: load_csv_data())
+button2.place(rely=0.65, relx=0.3)
+
+label_file = ttk.Label(file_frame, text="No File Selected")
+label_file.place(rely=0, relx=0)
+
+tv1 = ttk.Treeview(frame1)
+tv1.place(relheight=1, relwidth=1)
+
+treescrolly = tk.Scrollbar(frame1, orient="vertical", command=tv1.yview)
+treescrollx = tk.Scrollbar(frame1, orient="horizontal", command=tv1.xview)
+tv1.configure(xscrollcommand=treescrollx.set, yscrollcommand=treescrolly.set)
+treescrollx.pack(side="bottom", fill="x")
+treescrolly.pack(side="right", fill="y")
+
+def file_dialog():
+    filename = filedialog.askopenfilename(initialdir="/",
+                                          title="Select A File",
+                                          filetype=(("csv files", "*.csv"),("All Files", "*.*")))
+    label_file["text"] = filename
+    return None
+
+def load_csv_data():
+    file_path = label_file["text"]
+    try:
+        csv_filename = r"{}".format(file_path)
+        df = pd.read_csv(csv_filename)
+    except ValueError:
+        tk.messagebox.showerror("Information", "The file you have entered is invalid")
+        return None
+    except FileNotFoundError:
+        tk.messagebox.showerror("Information", f"No such file as {file_path}")
+        return None
+
+    clear_data()
+    tv1["column"] = list(df.columns)
+    tv1["show"] = "headings"
+    for column in tv1["columns"]:
+        tv1.heading(column, text=column)
+
+    df_rows = df.to_numpy().tolist()
+    for row in df_rows:
+        tv1.insert("", "end", values=row)
+    return None
+
+def clear_data():
+    tv1.delete(*tv1.get_children())
+
+
+
+root.mainloop()
