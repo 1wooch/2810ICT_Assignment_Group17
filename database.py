a7a3827bbac13e60ea851248cb5ad7ac5cf52352
MarQuerz <marco.querzola@griffithuni.edu.au>
2022-10-03 14:13:38 +1000

    Updated UI
    changed the ui design and added a search bar

--- database.py
+++ database.py
@@ -1,84 +1,58 @@
-# # import required libraries and packages
-# import csv
-# import sqlite3 as sql
-# import pandas as pd
-#
-# # set max row display
-# pd.options.display.max_rows = 2
-#
-# # must have dataset saved in C drive to use
-# dataset = pd.read_csv(r'C:\penalty_data_set.csv')
-
-# print(dataset)
-
-
-import tkinter as tk
-from tkinter import filedialog, messagebox, ttk
-
+from tkinter import *
+from tkinter import ttk
 import pandas as pd
+import tkinter as tk
 
-root = tk.Tk()
-root.geometry("500x500")
-root.pack_propagate(False)
-root.resizable(0, 0)
+def filterTreeView(*args):
+    ItemsOnTreeView = myTree.get_children()
 
-frame1 = tk.LabelFrame(root, text="CSV Data")
-frame1.place(height=250, width=500)
+    search = search_ent_var.get().capitalize()
 
-file_frame = tk.LabelFrame(root, text="Open File")
-file_frame.place(height=100, width=400, rely=0.65, relx=0)
+    for eachItem in ItemsOnTreeView:
+        if search in myTree.item(eachItem)['values'][2]:
+            search_var = myTree.item(eachItem)['values']
+            myTree.delete(eachItem)
 
-button1 = tk.Button(file_frame, text="Browse A File", command=lambda: file_dialog())
-button1.place(rely=0.65, relx=0.5)
+            myTree.insert("", 0, value=search_var)
 
-button2 = tk.Button(file_frame, text="Load File", command=lambda: load_csv_data())
-button2.place(rely=0.65, relx=0.3)
+root = Tk()
+root.title("NSW Traffic Penalty Data")
 
-label_file = ttk.Label(file_frame, text="No File Selected")
-label_file.place(rely=0, relx=0)
+topFrame = Frame(root, bg="white")
+topFrame.place(x=5, y=5, width=200, height=80)
 
-tv1 = ttk.Treeview(frame1)
-tv1.place(relheight=1, relwidth=1)
+treeFrame = Frame(root, bg="white")
+treeFrame.place(x=5, y=100, width=1500, height=800)
 
-treescrolly = tk.Scrollbar(frame1, orient="vertical", command=tv1.yview)
-treescrollx = tk.Scrollbar(frame1, orient="horizontal", command=tv1.xview)
-tv1.configure(xscrollcommand=treescrollx.set, yscrollcommand=treescrolly.set)
+lb1 = Label(topFrame, text="Search by", fg="black", bg="white")
+lb1.grid(row=0, column=0)
+search_ent_var = StringVar()
+myentry = Entry(topFrame, textvariable=search_ent_var)
+myentry.grid(row=0, column=1)
+search_ent_var.trace("w", filterTreeView)
+
+column = ['Financial Year', 'Month', 'Offence Code', 'Offence Description', 'Legislation', 'Section Clause',
+          'Penalty Amount', 'Camera Offence', 'Camera Type', 'Camera Location', 'Camera Location Details', 'School Zone',
+          'Speed Range', 'Speed Offence', 'Point to Point Offence', 'Red Light Camera Offence', 'Speed Camera Offence',
+          'Seatbelt Offence', 'Mobile Phone Offence', 'Parking Offence', 'Criminal Infringement Notice Scheme Offence',
+          'Food Safety Offence', 'Non-Motor Vehicle Offence', 'Number of Penalty Notices', 'Total Value of Penalty Notices']
+data = pd.read_csv("penalty_data_set_2.csv")
+data_rows = data.to_numpy().tolist()
+myTree = ttk.Treeview(treeFrame, height=100, column=column)
+myTree.place(relheight=1, relwidth=1)
+myTree['show']='headings'
+
+treescrolly = tk.Scrollbar(treeFrame, orient="vertical", command=myTree.yview)
+treescrollx = tk.Scrollbar(treeFrame, orient="horizontal", command=myTree.xview)
+myTree.configure(xscrollcommand=treescrollx.set, yscrollcommand=treescrolly.set)
 treescrollx.pack(side="bottom", fill="x")
 treescrolly.pack(side="right", fill="y")
 
-def file_dialog():
-    filename = filedialog.askopenfilename(initialdir="/",
-                                          title="Select A File",
-                                          filetype=(("csv files", "*.csv"),("All Files", "*.*")))
-    label_file["text"] = filename
-    return None
+for each in column:
+    myTree.column(each, width=80)
+    myTree.heading(each, text=each.capitalize())
 
-def load_csv_data():
-    file_path = label_file["text"]
-    try:
-        csv_filename = r"{}".format(file_path)
-        df = pd.read_csv(csv_filename)
-    except ValueError:
-        tk.messagebox.showerror("Information", "The file you have entered is invalid")
-        return None
-    except FileNotFoundError:
-        tk.messagebox.showerror("Information", f"No such file as {file_path}")
-        return None
-
-    clear_data()
-    tv1["column"] = list(df.columns)
-    tv1["show"] = "headings"
-    for column in tv1["columns"]:
-        tv1.heading(column, text=column)
-
-    df_rows = df.to_numpy().tolist()
-    for row in df_rows:
-        tv1.insert("", "end", values=row)
-    return None
-
-def clear_data():
-    tv1.delete(*tv1.get_children())
-
-
+for each in data_rows:
+    myTree.insert("", "end", values = each)
 
 root.mainloop()
