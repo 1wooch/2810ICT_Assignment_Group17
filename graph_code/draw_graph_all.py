6fc4efda41f376e392e292c0f63e111773cff976
Wonwoo Choi <onechoo1106@gmail.com>
2022-10-09 16:43:49 +1000

    graph and Ui finish except mobile

--- graph_code/draw_graph_all.py
+++ graph_code/draw_graph_all.py
@@ -43,29 +43,46 @@ class draw_graph(object):
         plt.plot(labels,y,linestyle='-',marker='o')
         plt.xticks(rotation=45)
         plt.show()
+        return plt.show()
 
-
-    def draw_2_line_graph(self,first_data,second_data,start_date,end_date): #for radar or camera
+    def draw_2_line_graph(self,first_data,second_data,start_date,end_date,schoolZoneBool): #for radar or camera
         
         start_date=str(start_date)
         end_date=str(end_date)
-        plt.figure(figsize=(20,3))  
-
-        plt.title(' Radar or Camera \n From %s to %s' %(start_date,end_date))
+        #plt.figure(figsize=(20,3))
+        plot1=plt.subplot2grid((3,3),(0,0),colspan=3)
+        plot2=plt.subplot2grid((3,3),(2,0),colspan=3)
+        # if schoolZoneBool:
+        #     plt.title(' Radar or Camera \n From %s to %s \n In School Zone' %(start_date,end_date))
+        # else:
+        #     plt.title(' Radar or Camera \n From %s to %s' %(start_date,end_date))
         labels=np.array(list(first_data.keys()))
         first_data=np.array(list(first_data.values())).astype(np.double)
         second_data=np.array(list(second_data.values())).astype(np.double)
-        plt.plot(labels,first_data,linestyle='-',marker='o',label="Radar")
-        plt.plot(labels,second_data,linestyle='-',marker='o',label="Camera")
+        plot1.plot(labels,first_data,linestyle='-',marker='o',label="Radar")
+        if schoolZoneBool:
+            plot1.set_title('Radar in School Zone')
+        else:
+            plot1.set_title('Radar')
+
+        plot2.plot(labels,second_data,linestyle='-',marker='o',label="Camera")
+        if schoolZoneBool:
+            plot2.set_title('Camera in School Zone')
+        else:
+            plot2.set_title('Camera')
         plt.legend(loc="upper left")
+        plt.xticks(rotation=45) #only apply on plot2
         plt.show()
 
-    def draw_pie_chart(self,result_dictionary,start_date,end_date,title):
+    def draw_pie_chart(self,result_dictionary,start_date,end_date,title,schoolZone):
         explode = [0, 0, 0, 0.1, 0.3] #pie chart distinguisher 
         start_date=str(start_date)
         end_date=str(end_date) 
         title=str(title)
-        plt.title(label='%s \n From %s to %s' %(title,start_date,end_date) )
+        if schoolZone:
+            plt.title(label='%s \n From %s to %s in School Zone' %(title,start_date,end_date) )
+        else:
+            plt.title(label='%s \n From %s to %s' %(title,start_date,end_date) )
         #plt.title('From ',start_date,' to ',end_date)
         #self.month_result=sorted(self.month_result.items(),key=lambda x:x[1],reverse=True)
         #sort the dictionary
